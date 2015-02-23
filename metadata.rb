82c8eb34f42ad2d657034ca4dc2256b42ca0d736
Mark Allen <mark@markcallen.com>
2015-02-22 17:09:21 -0800

    First checkin

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name             'uhostelk'
+maintainer       'Mark C Allen'
+maintainer_email 'mark@markcallen.com'
+license          'Apache 2.0'
+description      'Installs/Configures uhostelk'
+long_description 'Installs/Configures uhostelk'
+version          '0.1.0'
+
+%w{ ubuntu centos }.each do |os|
+	  supports os
+end
+
+%w{ apt elkstack }.each do |cb|
+	  depends cb
+end
---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: uhostelk
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "apt"
+
+include_recipe "elkstack::single"
