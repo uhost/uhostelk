48bf122f686d79e4b2277d7a4531312d115935b1
Mark C Allen <mark@markcallen.com>
2015-05-18 18:59:55 +0000

    Bumped to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mark@markcallen.com'
 license          'Apache 2.0'
 description      'Installs/Configures uhostelk'
 long_description 'Installs/Configures uhostelk'
-version          '0.1.0'
+version          '0.2.0'
 
 %w{ ubuntu centos }.each do |os|
 	  supports os
